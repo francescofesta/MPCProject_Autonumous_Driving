bd947b7d5f45fdfdbebc27349d8ab98842c4f1eb
volpicella95 <pac_23@hotmail.it>
2020-07-07 12:41:57 +0200

    errore_simulink_dimenisioni_outbus

---
+++ main.m
@@ -0,0 +1,21 @@
+clear all;
+clc;
+scenario_finale;
+Costmap;
+RRT;
+pose_record=record(scenario);
+
+sim_time={pose_record.SimulationTime};
+sim_time=cell2mat(sim_time');
+for i=1:1:size(pose_record,2)
+    
+    poses_new(i,1:2)=pose_record(i).ActorPoses(6).Position(1:2);
+    velocity_new(i,1)=sqrt((pose_record(i).ActorPoses(6).Velocity(1))^2+(pose_record(i).ActorPoses(6).Velocity(2))^2);
+    angle_new(i,1)=pose_record(i).ActorPoses(6).Yaw;
+end
+
+traiettoria_mat=[sim_time,poses_new,angle_new,velocity_new ]; 
+
+
+
+
