982f32493f5e0bd019c061c6c75395779e2bbc51
francescofesta <francesco@outlook.it>
2020-08-27 11:14:53 +0200

    sorpasso a sinistra non funzionante

--- ObstacleAvoidanceFcn.m
+++ ObstacleAvoidanceFcn.m
@@ -3,6 +3,7 @@ function cineq = ObstacleAvoidanceFcn(X,U,e,data,params)
 p=data.PredictionHorizon;
 d=sqrt(X(1,1)^2+X(1,2)^2);
 treshold_obstacle=3;
+ascissa=zeros(15,10);
   for i=1:1:(size(params.pos,1))
         
 %         argomento_vecnorm=[X(2:p+1,1)-params.pos(i,1)  X(2:p+1,2)-params.pos(i,2)];
@@ -16,6 +17,9 @@ treshold_obstacle=3;
 %         mat_trasf=[cos(X(1,3)) -sin(X(1,3)) 0 X(1,1);sin(X(1,3)) cos(X(1,3)) 0 X(1,2);0 0 1 0;0 0 0 1];
             posiz_ostacolo(:,j)=inv(mat_trasf(:,:,j))*[params.pos(i,1) params.pos(i,2) 0 1]';
             distanza_pitagora(i,j)=sqrt(posiz_ostacolo(1,j)^2+posiz_ostacolo(2,j)^2)-3;
+            if distanza_pitagora(i,j)<2 && i>5
+                ascissa(i,j)=posiz_ostacolo(1,j);
+            end
 %         dist_ost=vecnorm([X(1,1)-params.pos(i,1)  X(1,2)-params.pos(i,2)]);
         end
 %         if posiz_ostacolo(2,1)<0
@@ -41,10 +45,11 @@ treshold_obstacle=3;
 %         else vincolo_carr=zeros(p,1);
 %         end
     
-    end   
+   end   
     
 %   vincolo_carr=reshape(vincolo_carr,[],1);
 distanza_pitagora=reshape(distanza_pitagora,[],1);
-  cineq= [-distanza_pitagora];
+ascissa=reshape(ascissa,[],1);
+  cineq= [-distanza_pitagora; -ascissa]; %<=
 
 end
